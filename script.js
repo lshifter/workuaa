c79dfa78e98a5cfdb9dc7a6830d96eec7e5db564
lshifter <vitalykbondarenko0213@gmail.com>
2025-09-15 09:45:56 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const telegramLinks = [
+        'https://t.me/+LkynEFY4C49kZTUy',
+        'https://t.me/+6YWrsOwEe8k0NjNi',
+        'https://t.me/+HXfwanBPTP02MTBi'
+    ];
+
+    const applyButtons = document.querySelectorAll('.apply-button');
+
+    applyButtons.forEach(button => {
+        button.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent default link behavior
+            const randomIndex = Math.floor(Math.random() * telegramLinks.length);
+            const randomLink = telegramLinks[randomIndex];
+            window.open(randomLink, '_blank'); // Open link in a new tab
+        });
+    });
+});
