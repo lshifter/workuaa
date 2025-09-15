ac2b7848dc28c6b3e3f4ba9cfd3d0d0a0d4ce5c1
lshifter <vitalykbondarenko0213@gmail.com>
2025-09-15 10:11:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
     const telegramLinks = [
-        'https://t.me/+LkynEFY4C49kZTUy',
-        'https://t.me/+6YWrsOwEe8k0NjNi',
-        'https://t.me/+HXfwanBPTP02MTBi'
+        "https://t.me/+LkynEFY4C49kZTUy",
+        "https://t.me/+6YWrsOwEe8k0NjNi",
+        "https://t.me/+HXfwanBPTP02MTBi",
+        "https://t.me/+_-JX2UzLo8wxMDBi"
     ];
 
-    const applyButtons = document.querySelectorAll('.apply-button');
+    const applyButtons = document.querySelectorAll(".apply-button");
 
     applyButtons.forEach(button => {
-        button.addEventListener('click', (event) => {
+        button.addEventListener("click", (event) => {
             event.preventDefault(); // Prevent default link behavior
             const randomIndex = Math.floor(Math.random() * telegramLinks.length);
             const randomLink = telegramLinks[randomIndex];
-            window.open(randomLink, '_blank'); // Open link in a new tab
+            window.open(randomLink, "_blank"); // Open link in a new tab
         });
     });
 });
