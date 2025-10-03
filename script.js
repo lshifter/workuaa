15589c0586266248a66b8273577cd84ccaac63f8
lshifter <vitalykbondarenko0213@gmail.com>
2025-10-03 17:31:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -160,7 +160,7 @@ function renderJobs(jobsToRender) {
                 </div>
             </div>
             
-            <button class="apply-button" onclick="applyToJob("${job.telegramLink}", event)">
+            <button class="apply-button" onclick="applyToJob('${job.telegramLink}', event)">
                 <svg width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2">
                     <polygon points="13 2 3 14 12 14 11 22 21 10 12 10 13 2"/>
                 </svg>
